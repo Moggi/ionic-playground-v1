69310489348d87e342ab6d3bce9e1774a09bb838
Paulo Moggi <paulohpmoggi@gmail.com>
2015-06-01 18:59:05 -0300

    Experimento 1 - ionic w

--- www/js/app.js
+++ www/js/app.js
@@ -33,20 +33,43 @@ var update = function () {
 
     stats.end();
 
+    prevTime = Date.now();
     requestAnimationFrame( update );
 };
 
+var raio = 10;
+var quantia = 50;
+var x = raio;
+var y = raio;
+var velocity = 50;   // pixels/second
+
+var prevTime = Date.now();
+
 requestAnimationFrame( update );
 
+var circulos = [];
 
 function draw(){
     //limpa tela
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     //
-    ctx.beginPath();
-    ctx.arc(200,100,60,0,2*Math.PI);
-    ctx.fillStyle="gray";
-    ctx.fill();
+    // var tempX=0;
+    for( var i=0; i<quantia/2; i++)
+    {
+        var tempX = x+i*2*raio;
+        var tempY = 0;
+        for( var j=0; j<quantia/2; j++)
+        {
+            tempX %=canvasWidth;
+            tempY += 2*raio;
+            tempY %=canvasHeight;
+            ctx.beginPath();
+            ctx.arc( tempX, tempY, raio, 0, 2*Math.PI );
+            ctx.fillStyle="#555555";
+            ctx.fill();
+        }
+    }
+    x += velocity * deltaTime(prevTime);
 }
 
 function resizeCanvas(){
@@ -54,6 +77,10 @@ function resizeCanvas(){
     canvasHeight = window.innerHeight;
     canvas.setAttribute('width',canvasWidth);
     canvas.setAttribute('height',canvasHeight);
+}
+
+function deltaTime(anterior){
+    return (Date.now() - anterior)/1000;
 }
 
 
