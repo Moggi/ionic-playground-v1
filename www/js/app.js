cc47867aed297eda754c4844fafd9dd5a33cd029
Paulo Moggi <paulohpmoggi@gmail.com>
2015-06-01 16:44:43 -0300

    Add stats.min.js

--- www/js/app.js
+++ www/js/app.js
@@ -1,1 +1,70 @@
 
+
+window.onload = function(){
+
+var canvasWidth = window.innerWidth;
+var canvasHeight = window.innerHeight;
+
+var canvas = document.createElement('canvas');
+
+canvas.setAttribute('width',canvasWidth);
+canvas.setAttribute('height',canvasHeight);
+document.body.appendChild(canvas);
+
+var ctx = canvas.getContext('2d');
+
+window.addEventListener("resize", resizeCanvas);
+
+var stats = new Stats();
+stats.setMode(0); // 0: fps, 1: ms
+
+// align top-left
+stats.domElement.style.position = 'absolute';
+stats.domElement.style.left = '0px';
+stats.domElement.style.top = '0px';
+
+document.body.appendChild( stats.domElement );
+
+var update = function () {
+
+    stats.begin();
+
+    draw();
+
+    stats.end();
+
+    requestAnimationFrame( update );
+};
+
+requestAnimationFrame( update );
+
+
+function draw(){
+    //limpa tela
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    //
+    ctx.beginPath();
+    ctx.arc(200,100,60,0,2*Math.PI);
+    ctx.fillStyle="red";
+    ctx.fill();
+}
+
+function resizeCanvas(){
+    canvasWidth = window.innerWidth;
+    canvasHeight = window.innerHeight;
+    canvas.setAttribute('width',canvasWidth);
+    canvas.setAttribute('height',canvasHeight);
+}
+
+
+};
+
+
+
+
+
+
+
+
+
+//
