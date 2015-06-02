68a69b6298049ea969aabe06fa027d67955194e0
Paulo Moggi <paulohpmoggi@gmail.com>
2015-06-02 14:24:29 -0300

    Webgl Circulos fail

--- www/js/app.js
+++ www/js/app.js
@@ -12,6 +12,7 @@ canvas.setAttribute('height',canvasHeight);
 document.body.appendChild(canvas);
 
 var gl = canvas.getContext('webgl');
+gl.clearColor(1.0, 1.0, 1.0, 1.0);
 
 window.onresize = function(){ resizeCanvas(); };
 
@@ -34,21 +35,18 @@ var update = function () {
     stats.end();
 
     prevTime = Date.now();
-    // requestAnimationFrame( update );
+    requestAnimationFrame( update );
 };
 
-var raio = 10;
-var quantia = 50;
-var x = raio;
-var y = raio;
-var velocity = 50;   // pixels/second
 
-var prevTime = Date.now();
-
-requestAnimationFrame( update );
+    var prevTime = Date.now();
+    var raio = 10;
+    var quantia = 10;
+    var x = raio;
+    var y = raio;
+    var velocity = 50;   // pixels/second
 
 
-function draw(){
 
     var vertexShaderNode = createShaderFromScriptElement(gl, "node-vertex-shader");
     var fragmentShaderNode = createShaderFromScriptElement(gl, "node-fragment-shader");
@@ -59,26 +57,57 @@ function draw(){
     var ATTRIBUTES = 5;
     var j = 0;
     var data = [];
-    var circle = {x: 50, y: 50, r: 10};
-    var r = circle.r + 1;
 
-    data[j++] = (circle.x - r);
-    data[j++] = (circle.y - r);
-    data[j++] = circle.x;
-    data[j++] = circle.y;
-    data[j++] = r;
+    //=================================
+    for( var i=0; i<quantia/2; i++)
+    {
+        var tempX = x +i*2*raio;
+        tempX %=canvasWidth;
+        var tempY = 0;
+        for( var k=0; k<quantia/2; k++)
+        {
+            tempY += 2 * raio;
+            tempY %=canvasHeight;
 
-    data[j++] = (circle.x + (1 + Math.sqrt(2)) * r);
-    data[j++] = circle.y - r;
-    data[j++] = circle.x;
-    data[j++] = circle.y;
-    data[j++] = r;
+            data[j++] = (tempX - raio);
+            data[j++] = (tempY - raio);
+            data[j++] = tempX;
+            data[j++] = tempY;
+            data[j++] = raio;
 
-    data[j++] = (circle.x - r);
-    data[j++] = (circle.y + (1 + Math.sqrt(2)) * r);
-    data[j++] = circle.x;
-    data[j++] = circle.y;
-    data[j++] = r;
+            data[j++] = (tempX + (1 + Math.sqrt(2)) * raio);
+            data[j++] = tempY - raio;
+            data[j++] = tempX;
+            data[j++] = tempY;
+            data[j++] = raio;
+
+            data[j++] = (tempX - raio);
+            data[j++] = (tempY + (1 + Math.sqrt(2)) * raio);
+            data[j++] = tempX;
+            data[j++] = tempY;
+            data[j++] = raio;
+
+        }
+    }
+    //=================================
+
+    // data[j++] = (x - raio);
+    // data[j++] = (y - raio);
+    // data[j++] = x;
+    // data[j++] = y;
+    // data[j++] = raio;
+    //
+    // data[j++] = (x + (1 + Math.sqrt(2)) * raio);
+    // data[j++] = y - raio;
+    // data[j++] = x;
+    // data[j++] = y;
+    // data[j++] = raio;
+    //
+    // data[j++] = (x - raio);
+    // data[j++] = (y + (1 + Math.sqrt(2)) * raio);
+    // data[j++] = x;
+    // data[j++] = y;
+    // data[j++] = raio;
 
     var dataBuffer = new Float32Array(data);
 
@@ -87,15 +116,15 @@ function draw(){
     gl.bufferData(
         gl.ARRAY_BUFFER,
         dataBuffer,
-        gl.STATIC_DRAW);
+        gl.DYNAMIC_DRAW);
 
-        // set the resolution
     var resolutionLocation = gl.getUniformLocation(programNode, "u_resolution");
-    gl.uniform2f(resolutionLocation, canvas.width, canvas.height);
+    gl.uniform2f(resolutionLocation, canvasWidth, canvasHeight);
 
     var positionLocation = gl.getAttribLocation(programNode, "a_position");
     var centerLocation = gl.getAttribLocation(programNode, "a_center");
     var radiusLocation = gl.getAttribLocation(programNode, "a_radius");
+
 
     gl.enableVertexAttribArray(positionLocation);
     gl.enableVertexAttribArray(centerLocation);
@@ -105,29 +134,59 @@ function draw(){
     gl.vertexAttribPointer(centerLocation, 2, gl.FLOAT, false, ATTRIBUTES * Float32Array.BYTES_PER_ELEMENT, 8);
     gl.vertexAttribPointer(radiusLocation, 1, gl.FLOAT, false, ATTRIBUTES * Float32Array.BYTES_PER_ELEMENT, 16);
 
+
+requestAnimationFrame( update );
+
+function draw(){
+
+    gl.viewport(0, 0, canvasWidth, canvasHeight);
+    gl.uniform2f(resolutionLocation, canvasWidth, canvasHeight);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    //=================================
+    for( var i=0; i<quantia/2; i++)
+    {
+        var tempX = x +i*2*raio;
+        tempX %=canvasWidth;
+        var tempY = 0;
+        for( var k=0; k<quantia/2; k++)
+        {
+            tempY += 2 * raio;
+            tempY %=canvasHeight;
+
+            data[j++] = (tempX - raio);
+            data[j++] = (tempY - raio);
+            data[j++] = tempX;
+            data[j++] = tempY;
+            data[j++] = raio;
+
+            data[j++] = (tempX + (1 + Math.sqrt(2)) * raio);
+            data[j++] = tempY - raio;
+            data[j++] = tempX;
+            data[j++] = tempY;
+            data[j++] = raio;
+
+            data[j++] = (tempX - raio);
+            data[j++] = (tempY + (1 + Math.sqrt(2)) * raio);
+            data[j++] = tempX;
+            data[j++] = tempY;
+            data[j++] = raio;
+
+        }
+    }
+    //=================================
+
+    dataBuffer = new Float32Array(data);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(
+        gl.ARRAY_BUFFER,
+        dataBuffer,
+        gl.DYNAMIC_DRAW);
+
     gl.drawArrays(gl.TRIANGLES, 0, data.length/ATTRIBUTES);
 
-
-//     //limpa tela
-//     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-//     //
-//     // var tempX=0;
-//     for( var i=0; i<quantia/2; i++)
-//     {
-//         var tempX = x+i*2*raio;
-//         var tempY = 0;
-//         for( var j=0; j<quantia/2; j++)
-//         {
-//             tempX %=canvasWidth;
-//             tempY += 2*raio;
-//             tempY %=canvasHeight;
-//             ctx.beginPath();
-//             ctx.arc( tempX, tempY, raio, 0, 2*Math.PI );
-//             ctx.fillStyle="#555555";
-//             ctx.fill();
-//         }
-//     }
-//     x += velocity * deltaTime(prevTime);
+    x += velocity * deltaTime(prevTime);
 }
 
 function resizeCanvas(){
