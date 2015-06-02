7025b5912e6e89d81d21ce6c7e4c3a2cf5445b3e
Paulo Moggi <paulohpmoggi@gmail.com>
2015-06-02 16:58:33 -0300

    Circulos with PIXIjs

--- www/js/app.js
+++ www/js/app.js
@@ -11,9 +11,6 @@ canvas.setAttribute('width',canvasWidth);
 canvas.setAttribute('height',canvasHeight);
 document.body.appendChild(canvas);
 
-var gl = canvas.getContext('webgl');
-gl.clearColor(1.0, 1.0, 1.0, 1.0);
-
 window.onresize = function(){ resizeCanvas(); };
 
 var stats = new Stats();
@@ -40,160 +37,61 @@ var update = function () {
 
 
     var prevTime = Date.now();
+
     var raio = 10;
-    var quantia = 10;
+    var quantia = 50;
     var x = raio;
     var y = raio;
     var velocity = 50;   // pixels/second
 
+//================================================================================
 
 
-    var vertexShaderNode = createShaderFromScriptElement(gl, "node-vertex-shader");
-    var fragmentShaderNode = createShaderFromScriptElement(gl, "node-fragment-shader");
-    var programNode = createProgram(gl, [vertexShaderNode, fragmentShaderNode]);
-    gl.useProgram(programNode);
 
+// You can use either `new PIXI.WebGLRenderer`, `new PIXI.CanvasRenderer`, or `PIXI.autoDetectRenderer`
+var renderer = new PIXI.CanvasRenderer(canvasWidth,canvasHeight,{view:canvas,transparent:true});
 
-    var ATTRIBUTES = 5;
-    var j = 0;
-    var data = [];
+var stage = new PIXI.Container();
 
-    //=================================
-    for( var i=0; i<quantia/2; i++)
-    {
-        var tempX = x +i*2*raio;
-        tempX %=canvasWidth;
-        var tempY = 0;
-        for( var k=0; k<quantia/2; k++)
-        {
-            tempY += 2 * raio;
-            tempY %=canvasHeight;
+var graphics = new PIXI.Graphics();
 
-            data[j++] = (tempX - raio);
-            data[j++] = (tempY - raio);
-            data[j++] = tempX;
-            data[j++] = tempY;
-            data[j++] = raio;
-
-            data[j++] = (tempX + (1 + Math.sqrt(2)) * raio);
-            data[j++] = tempY - raio;
-            data[j++] = tempX;
-            data[j++] = tempY;
-            data[j++] = raio;
-
-            data[j++] = (tempX - raio);
-            data[j++] = (tempY + (1 + Math.sqrt(2)) * raio);
-            data[j++] = tempX;
-            data[j++] = tempY;
-            data[j++] = raio;
-
-        }
-    }
-    //=================================
-
-    // data[j++] = (x - raio);
-    // data[j++] = (y - raio);
-    // data[j++] = x;
-    // data[j++] = y;
-    // data[j++] = raio;
-    //
-    // data[j++] = (x + (1 + Math.sqrt(2)) * raio);
-    // data[j++] = y - raio;
-    // data[j++] = x;
-    // data[j++] = y;
-    // data[j++] = raio;
-    //
-    // data[j++] = (x - raio);
-    // data[j++] = (y + (1 + Math.sqrt(2)) * raio);
-    // data[j++] = x;
-    // data[j++] = y;
-    // data[j++] = raio;
-
-    var dataBuffer = new Float32Array(data);
-
-    var buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-    gl.bufferData(
-        gl.ARRAY_BUFFER,
-        dataBuffer,
-        gl.DYNAMIC_DRAW);
-
-    var resolutionLocation = gl.getUniformLocation(programNode, "u_resolution");
-    gl.uniform2f(resolutionLocation, canvasWidth, canvasHeight);
-
-    var positionLocation = gl.getAttribLocation(programNode, "a_position");
-    var centerLocation = gl.getAttribLocation(programNode, "a_center");
-    var radiusLocation = gl.getAttribLocation(programNode, "a_radius");
-
-
-    gl.enableVertexAttribArray(positionLocation);
-    gl.enableVertexAttribArray(centerLocation);
-    gl.enableVertexAttribArray(radiusLocation);
-
-    gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, ATTRIBUTES * Float32Array.BYTES_PER_ELEMENT, 0);
-    gl.vertexAttribPointer(centerLocation, 2, gl.FLOAT, false, ATTRIBUTES * Float32Array.BYTES_PER_ELEMENT, 8);
-    gl.vertexAttribPointer(radiusLocation, 1, gl.FLOAT, false, ATTRIBUTES * Float32Array.BYTES_PER_ELEMENT, 16);
-
+stage.addChild(graphics);
 
 requestAnimationFrame( update );
-
-function draw(){
-
-    gl.viewport(0, 0, canvasWidth, canvasHeight);
-    gl.uniform2f(resolutionLocation, canvasWidth, canvasHeight);
-    gl.clear(gl.COLOR_BUFFER_BIT);
-
-    //=================================
+function draw()
+{
+    graphics.clear();
+    graphics.beginFill(0x333333);
     for( var i=0; i<quantia/2; i++)
     {
         var tempX = x +i*2*raio;
         tempX %=canvasWidth;
         var tempY = 0;
-        for( var k=0; k<quantia/2; k++)
+        for( var j=0; j<quantia/2; j++)
         {
             tempY += 2 * raio;
             tempY %=canvasHeight;
 
-            data[j++] = (tempX - raio);
-            data[j++] = (tempY - raio);
-            data[j++] = tempX;
-            data[j++] = tempY;
-            data[j++] = raio;
-
-            data[j++] = (tempX + (1 + Math.sqrt(2)) * raio);
-            data[j++] = tempY - raio;
-            data[j++] = tempX;
-            data[j++] = tempY;
-            data[j++] = raio;
-
-            data[j++] = (tempX - raio);
-            data[j++] = (tempY + (1 + Math.sqrt(2)) * raio);
-            data[j++] = tempX;
-            data[j++] = tempY;
-            data[j++] = raio;
-
+            graphics.drawCircle(tempX,tempY,raio);
         }
     }
-    //=================================
 
-    dataBuffer = new Float32Array(data);
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-    gl.bufferData(
-        gl.ARRAY_BUFFER,
-        dataBuffer,
-        gl.DYNAMIC_DRAW);
-
-    gl.drawArrays(gl.TRIANGLES, 0, data.length/ATTRIBUTES);
+    // this is the main render call that makes pixi draw your container and its children.
+    renderer.render(graphics);
 
     x += velocity * deltaTime(prevTime);
 }
+
+
+
+
 
 function resizeCanvas(){
     canvasWidth = window.innerWidth;
     canvasHeight = window.innerHeight;
     canvas.setAttribute('width',canvasWidth);
     canvas.setAttribute('height',canvasHeight);
+    renderer.resize(canvasWidth,canvasHeight);
 }
 
 function deltaTime(anterior){
